b9fedccf2185252332a7b9bee134dcdac84558db
Onomsgreatness <174793502+Onomsgreatness@users.noreply.github.com>
2025-12-31 16:53:15 +0000

    Update GP.java

--- src/model/GP.java
+++ src/model/GP.java
@@ -1,12 +1,17 @@
+/**
+ * Author: Onome Abuku <oa22aed@herts.ac.uk>
+ *     ID: 21092431
+ *     References: Dr. John Kanyaru, BookShop Example.
+ */
+
 package model;
 
 import java.util.Date;
-import java.text.SimpleDateFormat;
 
 public class GP extends Clinician {
 
     public GP(String clinicianId, String firstName, String lastName, String title, String speciality,
-              int gmcNumber, String contact,String email,String workplaceId,
+              String gmcNumber, String contact,String email,String workplaceId,
               String workplaceType, EmploymentStatus employmentStatus, Date startDate){
         super(clinicianId, firstName, lastName, title, speciality,
                 gmcNumber, contact, email, workplaceId,
